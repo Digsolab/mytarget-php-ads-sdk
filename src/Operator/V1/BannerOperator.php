30b1b8297346cb58ea588e3a81a6fa3b5c6eb60a
nikita2206 <inefedor@gmail.com>
2016-06-28 00:09:02 +0300

    edit methods for Banner

--- src/Operator/V1/BannerOperator.php
+++ src/Operator/V1/BannerOperator.php
@@ -46,16 +46,56 @@ class BannerOperator
      * @param Banner[] $banners
      * @param array|null $context
      *
-     * @return Banner[]
+     * @return Banner[] Created Banner objects with all fields filled
      */
     public function createAll($campaignId, array $banners, array $context = null)
     {
+        $context = (array)$context + ["limit-by" => "banner-create"];
         $rawBanners = array_map(function (Banner $banner) {
             return $this->mapper->snapshot($banner);
         }, array_values($banners));
 
         $path = sprintf("/api/v1/campaigns/%d/banners.json", $campaignId);
         $json = $this->client->post($path, null, $rawBanners, $context);
+
+        return array_map(function ($json) {
+            return $this->mapper->hydrateNew(Banner::class, $json);
+        }, $json);
+    }
+
+    /**
+     * @param Banner $banner
+     * @param BannerFields|null $fields
+     * @param array|null $context
+     *
+     * @return Banner|null
+     */
+    public function edit(Banner $banner, BannerFields $fields = null, array $context = null)
+    {
+        $banners = $this->editAll([$banner], $fields, $context);
+
+        return $banners ? reset($banners) : null;
+    }
+
+    /**
+     * @param Banner[] $banners
+     * @param BannerFields|null $fields Which fields to return in response
+     * @param array|null $context
+     *
+     * @return Banner[]
+     */
+    public function editAll(array $banners, BannerFields $fields = null, array $context = null)
+    {
+        $context = (array)$context + ["limit-by" => "banner-create"];
+        $fields = $fields ?: BannerFields::create();
+
+        $banners = array_values($banners);
+        $ids = array_map(function (Banner $b) { return $b->getId(); }, $banners);
+        $rawBanners = array_map([$this->mapper, "snapshot"], $banners);
+
+        $query = ["fields" => $this->mapFields($fields->getFields())];
+        $path = sprintf("/api/v1/banners/%s.json", implode(";", $ids));
+        $json = $this->client->post($path, $query, $rawBanners, $context);
 
         return array_map(function ($json) {
             return $this->mapper->hydrateNew(Banner::class, $json);
