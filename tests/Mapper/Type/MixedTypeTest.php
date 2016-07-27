3cbc6202f2f6dbc7d2c5751e819173db5ed93018
Alexander Novikov <a.novikov@digsolab.com>
2016-07-27 16:14:49 +0300

    mixed type test

--- tests/Mapper/Type/MixedTypeTest.php
+++ tests/Mapper/Type/MixedTypeTest.php
@@ -2,7 +2,39 @@
 
 namespace MyTarget\Mapper\Type;
 
+use MyTarget\Mapper\Mapper;
+
 class MixedTypeTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var \PHPUnit_Framework_MockObject_MockObject|RateLimitProvider */
+    protected $mapper;
 
+    protected function setUp()
+    {
+        $this->mapper = $this->getMockBuilder(Mapper::class)
+                             ->disableOriginalConstructor()
+                             ->getMock();
+    }
+
+    public function testItHydrates()
+    {
+        $mixedType = new MixedType();
+
+        $value = new \stdClass();
+
+        $result = $mixedType->hydrated($value, 'any type', $this->mapper);
+
+        $this->assertSame($value, $result);
+    }
+
+    public function testItHMakesSnapshot()
+    {
+        $mixedType = new MixedType();
+
+        $value = new \stdClass();
+
+        $result = $mixedType->snapshot($value, 'any type', $this->mapper);
+
+        $this->assertSame($value, $result);
+    }
 }
