bc05ab8faa38d923c8a548fe240134edd75ab708
Vlad Maslennikov <v.maslennikov@digsolab.com>
2016-08-02 17:25:54 +0300

    code

--- tests/Transport/Middleware/Impl/RequestResponseLoggerMiddlewareTest.php
+++ tests/Transport/Middleware/Impl/RequestResponseLoggerMiddlewareTest.php
@@ -37,30 +37,31 @@ class RequestResponseLoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
 
         $response = $this->getMock(ResponseInterface::class);
         $response->method('getHeaders')->willReturn(['X-Ok' => ['1']]);
-        $responseStream = new Stream(fopen('php://temp', 'r+'));
-        $responseStream->write('{"json": true}');
-        $responseStream->seek(0);
-        $response->method('getBody')->willReturn($responseStream);
+        $response->method('getBody')->willReturn(call_user_func(function() {
+            $responseStream = new Stream(fopen('php://temp', 'r+'));
+            $responseStream->write('{"json": true}');
+            $responseStream->seek(0);
+
+            return $responseStream;
+        }));
         $response->method('getProtocolVersion')->willReturn('1.1');
         $response->method('getStatusCode')->willReturn(200);
 
         /** @var HttpMiddlewareStack|\PHPUnit_Framework_MockObject_MockObject $stack */
         $stack = $this->getMockBuilder(HttpMiddlewareStack::class)->disableOriginalConstructor()->getMock();
 
-        $logger->expects($this->at(0))
+        $logger->expects(self::at(0))
             ->method('info')
-            ->with("MyTarget request:\nProtocol: 1.1\nMethod: GET\nURI: /\nHeaders:\nX-Phpunit: a\nX-Phpunit: b\nBody: some request data",
-                   $context ?: []);
+            ->with("MyTarget request:\nProtocol: 1.1\nMethod: GET\nURI: /\nHeaders:\nX-Phpunit: a\nX-Phpunit: b\nBody: some request data", $context ?: []);
 
-        $stack->expects($this->once())
+        $stack->expects(self::once())
           ->method('request')
           ->with($request, $context)
           ->willReturn($response);
 
-        $logger->expects($this->at(1))
+        $logger->expects(self::at(1))
            ->method('info')
-           ->with("MyTarget response:\nProtocol: 1.1\nCode: 200\nHeaders:\nX-Ok: 1\nBody: {\"json\": true}",
-                  $context ?: []);
+           ->with("MyTarget response:\nProtocol: 1.1\nCode: 200\nHeaders:\nX-Ok: 1\nBody: {\"json\": true}", $context ?: []);
 
         $middleware->request($request, $stack, $context);
     }
