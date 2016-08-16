Digsolab/mytarget-php-ads-sdk

commit 8d757a9c8a3a092110a1b729b4a429c5f0658c99

fix #31 convert singular json objects to arrays of one object

src/functions.php
@@ -104,3 +104,16 @@ function date_mutable(\DateTimeInterface $dt)
 
     return $mutable;
 }
+
+/**
+ * The API will give us `{...}`, instead of `[{...}]` in the response
+ * if we select the object by ID and give it only one ID.
+ *
+ * @param array $json
+ * @param int $howMuch
+ * @return array
+ */
+function objects_array_fixup($json, $howMuch)
+{
+    return $howMuch > 1 ? $json : [$json];
+}

src/Operator/V1/BannerOperator.php
@@ -8,6 +8,7 @@ use Dsl\MyTarget\Domain\V1\Banner\BannerStat;
 use Dsl\MyTarget\Domain\V1\Enum\Status;
 use Dsl\MyTarget\Mapper\Mapper;
 use Dsl\MyTarget\Operator\V1\Fields\BannerFields;
+use Dsl\MyTarget as f;
 
 class BannerOperator
 {
@@ -155,6 +156,7 @@ class BannerOperator
 
         $path = sprintf("/api/v1/banners%s.json", $ids ? "/" . implode(";", $ids) : "");
         $json = $this->client->get($path, $query, $context);
+        $json = f\objects_array_fixup($json, count($ids));
 
         return array_map(function ($json) {
             return $this->mapper->hydrateNew(BannerStat::class, $json);

src/Operator/V1/CampaignOperator.php
@@ -9,6 +9,7 @@ use Dsl\MyTarget\Domain\V1\Enum\Status;
 use Dsl\MyTarget\Mapper\Mapper;
 use Dsl\MyTarget\Operator\V1\Fields\CampaignFields;
 use Dsl\MyTarget\Client;
+use Dsl\MyTarget as f;
 
 class CampaignOperator
 {
@@ -142,6 +143,7 @@ class CampaignOperator
 
         $path = sprintf("/api/v1/campaigns/%s.json", implode(";", $ids));
         $json = $this->client->get($path, $query, $context);
+        $json = f\objects_array_fixup($json, count($ids));;
 
         return array_map(function ($json) {
             return $this->mapper->hydrateNew(CampaignStat::class, $json);

src/Operator/V1/DetailedExactDemographyOperator.php
@@ -6,6 +6,7 @@ use Dsl\MyTarget\Client;
 use Dsl\MyTarget\Domain\DateRange;
 use Dsl\MyTarget\Domain\V1\Demography\DetailedExactDemographyCampaigns;
 use Dsl\MyTarget\Mapper\Mapper;
+use Dsl\MyTarget as f;
 
 class DetailedExactDemographyOperator
 {
@@ -48,6 +49,7 @@ class DetailedExactDemographyOperator
         $path = sprintf('/api/v1/statistics/campaigns/%s/detailed_exact_demography.json', implode(';', $ids));
 
         $json = $this->client->get($path, $query, $context);
+        $json = f\objects_array_fixup($json, count($ids));
 
         return $this->mapper->hydrateNew(DetailedExactDemographyCampaigns::class, $json);
     }

src/Operator/V1/StatisticOperator.php
@@ -10,6 +10,7 @@ use Dsl\MyTarget\Domain\V1\Statistic\ObjectDailyStat;
 use Dsl\MyTarget\Domain\V1\Statistic\ObjectHourlyStat;
 use Dsl\MyTarget\Domain\V1\Statistic\ObjectStat;
 use Dsl\MyTarget\Mapper\Mapper;
+use Dsl\MyTarget as f;
 
 class StatisticOperator
 {
@@ -72,6 +73,7 @@ class StatisticOperator
     {
         $path = $this->path($ids, $objectType, $statType, $datesPredicate);
         $json = $this->client->get($path, null, $context);
+        $json = f\objects_array_fixup($json, count($ids));
 
         $type = $statType === StatisticType::day() ? ObjectDailyStat::class : ObjectHourlyStat::class;
         return array_map(function ($json) use ($type) {

commit 9229517157f8b711731dce1c4ecf021ca67c89ec

fix bug introduced in 8d757a9c where if empty array as IDs was passed we would wrap the result in an array

src/functions.php
@@ -115,5 +115,5 @@ function date_mutable(\DateTimeInterface $dt)
  */
 function objects_array_fixup($json, $howMuch)
 {
-    return $howMuch > 1 ? $json : [$json];
+    return $howMuch === 1 ? [$json] : $json;
 }