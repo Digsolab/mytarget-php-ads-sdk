b0b894e194597d9bef931f41e7277eb8feb08596
Alexander Novikov <a.novikov@digsolab.com>
2017-01-25 13:57:05 +0300

    method annotation fixed

--- src/Operator/V1/Fields/UserApiFields.php
+++ src/Operator/V1/Fields/UserApiFields.php
@@ -4,29 +4,28 @@ namespace Dsl\MyTarget\Operator\V1\Fields;
 
 use Dsl\MyTarget\Operator\AbstractFields;
 
+/**
+ * @method UserApiFields withId()
+ * @method UserApiFields withUsername()
+ * @method UserApiFields withFirstName()
+ * @method UserApiFields withLastName()
+ * @method UserApiFields withEmail()
+ * @method UserApiFields withStatus()
+ * @method UserApiFields withAdditionalInfo()
+ * @method UserApiFields withMailings()
+ * @method UserApiFields withPermissions()
+ * @method UserApiFields withAccount()
+ * @method UserApiFields withAgency()
+ * @method UserApiFields withAppendUtm()
+ * @method UserApiFields withShowCompactView()
+ * @method UserApiFields withInfoCurrency()
+ * @method UserApiFields withCurrency()
+ * @method UserApiFields withTypes()
+ * @method UserApiFields withPartnerApproved()
+ * @method UserApiFields withLanguage()
+ */
 class UserApiFields extends AbstractFields
 {
-    /**
-     * @method UserApiFields withId()
-     * @method UserApiFields withUsername()
-     * @method UserApiFields withFirstName()
-     * @method UserApiFields withLastName()
-     * @method UserApiFields withEmail()
-     * @method UserApiFields withStatus()
-     * @method UserApiFields withAdditionalInfo()
-     * @method UserApiFields withMailings()
-     * @method UserApiFields withPermissions()
-     * @method UserApiFields withAccount()
-     * @method UserApiFields withAgency()
-     * @method UserApiFields withAppendUtm()
-     * @method UserApiFields withShowCompactView()
-     * @method UserApiFields withInfoCurrency()
-     * @method UserApiFields withCurrency()
-     * @method UserApiFields withTypes()
-     * @method UserApiFields withPartnerApproved()
-     * @method UserApiFields withLanguage()
-     */
-
     public function defaultFields()
     {
         return [
